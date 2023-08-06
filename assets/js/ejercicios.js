546bc7fca67972f10ea2471f74b35266a673341c
Osmaryz <100032785+Osmaryz@users.noreply.github.com>
2023-08-06 16:30:59 -0400

    primera carga

---
+++ assets/js/ejercicios.js
@@ -0,0 +1,61 @@
+/* Ejercicios
+
+edad = prompt ("Ingrese un número");
+if (edad < 18) {
+console.log("1") // Instrucción 1
+console.log("2") // Instrucción 2
+console.log("3") // Instrucción 3
+a = 2 + 2 // Instrucción 4
+console.log(a) // Instrucción 5
+} else {
+    console.log("Su edad supera a los 18");
+}*/
+
+/*temperatura = prompt ("Introduzca la temperatura el Celsius");
+if (temperatura >30 ) {
+    alert("Esta haciendo mucho calor")
+} else {
+    alert ("Amigo tenemos una temperatura agradable")
+}*/
+
+/* Operadores lógicos
+
+== igual
+!= no igual/ distinto
+> mayor a 
+< menos a 
+>= mayor o igual a 
+>= menor o igual a 
+=== estrictamente igual 
+!== estrictamente distinto
+&& Si ambas condiciones son ciertas se resuelve como true. Si alguna de ellas es falsa se resuelve como false
+||   Si alguna de las expresiones es cierta se resuelve como true. si ambas son falsas se resuelve como false
+! Invierte la expresión. No true es false, no false es true.
+
+
+*/
+
+
+/*password = prompt ("Si concoces el password ingresalo")
+if (password == 'abracadabra'){
+alert('Conoces el password');
+} else {
+    alert ("Sigue intentando");
+}*/
+
+/*
+pedazosDeTorta = 9
+invitados = 12
+cantidadDePedazosPorInvitado = pedazosDeTorta / invitados
+if (cantidadDePedazosPorInvitado >= 1) {
+alert("Hay suficientes pedazos de torta para todos 🎉");
+} else {
+alert("Hay más invitados que pedazos de torta 😥");
+};
+*/
+
+/*function agregarBorde() {
+    imagenSinBorde = document.getElementById('imagen_sin_borde');
+    imagenSinBorde.style.border = '2px solid red';
+  }*/
+
